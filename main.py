a120f91ecccfdfe60f7f6c20866a9ef27e4a9f92
Alvaro Alvarado <alvaro@quinta.tech>
2025-04-24 13:55:35 -0600

    Refactor code formatting in main.py for improved readability and consistency

--- main.py
+++ main.py
@@ -54,48 +54,48 @@ if __name__ == "__main__":
 
     menu()
     choice = input("Option: ")
-    if choice == "1":  # Get access token
+    if choice == "1":       # Get access token
         access_token = tire_api.get_access_token()
         print("Access Token:", access_token)
-    elif choice == "2":  # Get vehicle list
+    elif choice == "2":     # Get vehicle list
         vehicle_list = tire_api.get_vehicle_list()
         print("Vehicle List:", vehicle_list)
-    elif choice == "3":  # Get vehicle info
+    elif choice == "3":     # Get vehicle info
         vehicle_id = input("Enter Vehicle ID: ")
         vehicle_info = tire_api.get_vehicle_info(vehicle_id=vehicle_id)
         print("Vehicle Info:", vehicle_info)
-    elif choice == "4":  # Get tire list
+    elif choice == "4":     # Get tire list
         tire_list = tire_api.get_tire_list()
         print("Tire List:", tire_list)
-    elif choice == "5":  # Get tires info by vehicle
+    elif choice == "5":     # Get tires info by vehicle
         vehicle_id = input("Enter Vehicle ID: ")
         tires_info = tire_api.get_tires_info_by_vehicle(vehicle_id=vehicle_id)
         print("Tires Info by Vehicle:", tires_info)
-    elif choice == "6":  # Get tire info
+    elif choice == "6":     # Get tire info
         tire_id = input("Enter Tire ID: ")
         tire_info = tire_api.get_tire_info(tire_id=tire_id)
         print("Tire Info:", tire_info)
-    elif choice == "7":  # Get sensor info
+    elif choice == "7":     # Get sensor info
         sensor_id = input("Enter Sensor ID: ")
         sensor_info = tire_api.get_sensor_info(sensor_id=sensor_id)
         print("Sensor Info:", sensor_info)
-    elif choice == "8":  # Get tbox info
+    elif choice == "8":     # Get tbox info
         tbox_id = input("Enter Tbox ID: ")
         tbox_info = tire_api.get_tbox_info(tbox_id=tbox_id)
         print("Tbox Info:", tbox_info)
-    elif choice == "9":  # Get tire brands
+    elif choice == "9":     # Get tire brands
         tire_brands = tire_api.get_tire_brands()
         print("Tire Brands:", tire_brands)
-    elif choice == "10":  # Get tire sizes
+    elif choice == "10":    # Get tire sizes
         tire_sizes = tire_api.get_tire_sizes()
         print("Tire Sizes:", tire_sizes)
-    elif choice == "11":  # Get vehicle models
+    elif choice == "11":    # Get vehicle models
         vehicle_models = tire_api.get_vehicle_models()
         print("Vehicle Models:", vehicle_models)
-    elif choice == "12":  # Get axle types
+    elif choice == "12":    # Get axle types
         axle_types = tire_api.get_axle_types()
         print("Axle Types:", axle_types)
-    elif choice == "13":  # Add vehicle
+    elif choice == "13":    # Add vehicle
         new_vehicle = {
             "isTractor": 0,
             "licensePlateNumber": "ABC123",
@@ -107,7 +107,7 @@ if __name__ == "__main__":
         }
         response = tire_api.add_vehicle(new_vehicle)
         print("Add Vehicle Response:", response)
-    elif choice == "14":  # Update vehicle
+    elif choice == "14":    # Update vehicle
         updated_vehicle = {
             "id": "7609",
             "isTractor": 0,
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         }
         response = tire_api.update_vehicle(updated_vehicle)
         print("Update Vehicle Response:", response)
-    elif choice == "15":  # Add tire
+    elif choice == "15":    # Add tire
         new_tire = {
             "tyreCode": "ABC123",
             "tyreBrandId": "1",
@@ -130,7 +130,7 @@ if __name__ == "__main__":
         }
         response = tire_api.add_tire(new_tire)
         print("Add Tire Response:", response)
-    elif choice == "16":  # Update tire
+    elif choice == "16":    # Update tire
         updated_tire = {
             "id": "47414",
             "tyreCode": "ABC123",
@@ -141,16 +141,14 @@ if __name__ == "__main__":
         }
         response = tire_api.update_tire(updated_tire)
         print("Update Tire Response:", response)
-    elif choice == "17":
-        # Add sensor
+    elif choice == "17":    # Add sensor
         new_sensor = {
             "sensorCode": "A4C1386C7403",
             "version": "2.5.0",
         }
         response = tire_api.add_sensor(new_sensor)
         print("Add Sensor Response:", response)
-    elif choice == "18":
-        # Update sensor
+    elif choice == "18":    # Update sensor
         updated_sensor = {
             "id": "49356",
             "sensorCode": "A4C1386C7403",
@@ -160,15 +158,13 @@ if __name__ == "__main__":
         }
         response = tire_api.update_sensor(updated_sensor)
         print("Update Sensor Response:", response)
-    elif choice == "19":
-        # Add Tbox
+    elif choice == "19":    # Add Tbox
         new_tbox = {
             "tboxCode": "A9C1386C7403",
         }
         response = tire_api.add_tbox(new_tbox)
         print("Add Tbox Response:", response)
-    elif choice == "20":
-        # Update Tbox
+    elif choice == "20":    # Update Tbox
         updated_tbox = {
             "id": "8756",
             "tboxCode": "A9C1386C7403",
@@ -176,15 +172,13 @@ if __name__ == "__main__":
         }
         response = tire_api.update_tbox(updated_tbox)
         print("Update Tbox Response:", response)
-    elif choice == "21":
-        # Get Tbox List
+    elif choice == "21":    # Get Tbox List
         tbox_list = tire_api.get_tboxes_list()
         print("Tbox List:", tbox_list)
-    elif choice == "22":
-        # Get Sensor List
+    elif choice == "22":    # Get Sensor List
         sensor_list = tire_api.get_sensor_list()
         print("Sensor List:", sensor_list)
-    elif choice == "23":
+    elif choice == "23":    # Bind tire to vehicle
         vehicle_id = input("Enter Vehicle ID: ")
         tire_id = input("Enter Tire ID: ")
         axle_idx = input("Enter Axle Index: ")
@@ -193,7 +187,7 @@ if __name__ == "__main__":
             vehicle_id, tire_id, axle_idx, wheel_id
         )
         print("Bind Tire Response:", response)
-    elif choice == "24":
+    elif choice == "24":    # Unbind tire from vehicle
         vehicle_id = input("Enter Vehicle ID: ")
         tire_id = input("Enter Tire ID: ")
         response = tire_api.unbind_tire_from_vehicle(vehicle_id, tire_id)
